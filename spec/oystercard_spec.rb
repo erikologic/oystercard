d4df06c6153c6c3d9b088f5174da039a0b147cff
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-01-09 16:26:26 +0000

    Create oyster card class and balance attribute

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
-end
+require "oystercard"
 
+describe Oystercard do
+  context "when initialized" do
+    it "has a balance" do
+      expect(subject.balance).to eq 0
+    end
+  end
+end
