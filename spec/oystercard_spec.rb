a3f6ba3e163484f9d6dccf80de98a57c4fc16e5e
Enrico Graziani <mrenrich84@gmail.com>
2017-01-09 16:36:03 +0000

    OysterCard: add topup method, refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,13 +6,15 @@ describe OysterCard do
       expect(subject.balance).to eq 0
     end
   end
-  context "when adding money" do
-    before do
-      @topup = rand(50)
-      subject.topup(@topup)
-    end
-    it "has balanced increased by #{@topup}" do
-      expect(subject.balance).to eq @topup
+  describe ".topup" do
+    context "when adding money" do
+      before do
+        @topup = rand(50)
+        subject.topup(@topup)
+      end
+      it "has balanced increased by #{@topup}" do
+        expect(subject.balance).to eq @topup
+      end
     end
   end
 end
