0ba846b804cd58c52ffb496a5009cbf278e3b1d3
Enrico Graziani <mrenrich84@gmail.com>
2017-01-09 16:34:05 +0000

    OysterCard: add topup method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,11 @@
-class Oystercard
+class OysterCard
   attr_reader :balance
+  
   def initialize
     @balance = 0
+  end
+
+  def topup(value)
+    @balance += value
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,18 @@
 require "oystercard"
 
-describe Oystercard do
+describe OysterCard do
   context "when initialized" do
     it "has a balance" do
       expect(subject.balance).to eq 0
+    end
+  end
+  context "when adding money" do
+    before do
+      @topup = rand(50)
+      subject.topup(@topup)
+    end
+    it "has balanced increased by #{@topup}" do
+      expect(subject.balance).to eq @topup
     end
   end
 end
