4322a69113ead2ddc7bc4fbc64f8b36e2c153f55
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-01-09 17:07:38 +0000

    Create a Max capicity limit for the balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class OysterCard
   attr_reader :balance
-  
+  MAX_CAPACITY = 90
+
   def initialize
     @balance = 0
   end
 
-  def topup(value)
+  def top_up(value)
+    raise "Maximum limit of #{MAX_CAPACITY} reached" if @balance + value > MAX_CAPACITY
     @balance += value
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,5 @@
 require "oystercard"
+require "pry"
 
 describe OysterCard do
   context "when initialized" do
@@ -6,14 +7,23 @@ describe OysterCard do
       expect(subject.balance).to eq 0
     end
   end
-  describe ".topup" do
+  describe ".top_up" do
     context "when adding money" do
       before do
-        @topup = rand(50)
-        subject.topup(@topup)
+        @top_up = rand(50)
+        subject.top_up(@top_up)
       end
-      it "has balanced increased by #{@topup}" do
-        expect(subject.balance).to eq @topup
+      it "can have balance increase" do
+        expect(subject.balance).to eq @top_up
+      end
+    end
+    context "when exceeding maximum limit" do
+      before do
+        @max_capacity = OysterCard::MAX_CAPACITY
+        subject.top_up(@max_capacity)
+      end
+      it "raises 'Maximum limit reached' error" do
+        expect{subject.top_up(rand(10))}.to raise_error("Maximum limit of #{@max_capacity} reached")
       end
     end
   end
